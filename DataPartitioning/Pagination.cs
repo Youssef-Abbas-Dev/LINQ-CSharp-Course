a70c6fd55c24179cbd5d0c877ba4a106d7ba38a0
Youssef Abbas <youssefabbasawad@gmail.com>
2024-06-25 20:33:54 +0330

    Pagination class added

---
+++ DataPartitioning/Pagination.cs
@@ -0,0 +1,33 @@
+﻿namespace DataPartitioning
+{
+    public class Pagination<T> where T : class
+    {
+        private readonly int _dataPerPage;
+        public Pagination(int dataPerPage)
+        {
+            _dataPerPage = dataPerPage;
+        }
+
+        public IEnumerable<T> Paginate(IEnumerable<T> collection)
+        {
+            var pageNumber = GetPageNumber();
+
+            Console.WriteLine($"{typeof(T).Name}s For Page Number {pageNumber}");
+            var data = collection
+              .Skip(_dataPerPage * (pageNumber - 1))
+              .Take(_dataPerPage);
+
+            return data;
+        }
+
+        private int GetPageNumber()
+        {
+            Console.Write("Please enter page number: ");
+            bool validPageNumber = int.TryParse(Console.ReadLine(), out int pageNumber);
+            if (validPageNumber)
+                return pageNumber;
+
+            return 1;
+        }
+    }
+}
--- DataPartitioning/Program.cs
+++ DataPartitioning/Program.cs
@@ -1,4 +1,29 @@
 ﻿// Data Partitioning with Linq
+// Skip(), SkipWhile(), SkipLast()
+// Take(), TakeWhile(), TakeLast()
+// Chunk()
+using DataPartitioning;
+
+var articles = Article.FetchArticles();
+var pagination = new Pagination<Article>(5);
+
+var data = pagination.Paginate(articles);
+
+foreach (var article in data)
+    Console.WriteLine($"ID: {article.Id}, TITLE: {article.Title}, DESCRIPTION: {article.Description}");
+
+
+
+
+//var data = articles.Chunk(10).ToList();
+//Console.WriteLine($"{data.Count()} Chunks\n");
+
+//for (int i = 0; i < data.Count(); i++)
+//{
+//    Console.WriteLine($"Chunk: {i + 1}");
+//    foreach (var article in data[i])
+//        Console.WriteLine($"ID: {article.Id}, TITLE: {article.Title}, DESCRIPTION: {article.Description}");
+//}
 
 
 Console.ReadKey();
