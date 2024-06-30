2042c69968d7c2adafe68b0c0d6e0a8b9053b29d
Youssef Abbas <youssefabbasawad@gmail.com>
2024-06-30 08:50:12 +0330

    Linq query operators lesson

---
+++ LinqQuaryOperators/Program.cs
@@ -0,0 +1,18 @@
+﻿using LinqQuaryOperators;
+var students = Student.GetStudents();
+
+// LINQ Extension Methods (Method Syntax)
+students.Where(s => s.Age > 25).OrderByDescending(s => s.Age);
+
+//Enumerable.Where(students, s => s.Age > 25);
+
+// LINQ Query Operators (Query Syntax)
+var data = (from s in students
+            where s.Age > 25
+            orderby s.Age descending
+            select s).ToList();
+
+foreach (var student in data)
+    Console.WriteLine($"Name: {student.Name}, Age: {student.Age}");
+
+Console.ReadKey();
---
+++ LinqQuaryOperators/Student.cs
@@ -0,0 +1,25 @@
+﻿namespace LinqQuaryOperators
+{
+    public class Student
+    {
+        public required int Id { get; set; }
+        public required string Name { get; set; }
+        public required int Age { get; set; }
+        public required string Country { get; set; }
+
+        public static List<Student> GetStudents()
+        {
+            return new List<Student>()
+            {
+              new Student { Id = 1, Age = 18, Name = "Hasan", Country = "Egypt" },
+              new Student { Id = 2, Age = 21, Name = "Salah", Country = "Iraq" },
+              new Student { Id = 3, Age = 27, Name = "Ahmed", Country = "Egypt" },
+              new Student { Id = 4, Age = 31, Name = "Saleh", Country = "Qatar" },
+              new Student { Id = 5, Age = 35, Name = "Basem", Country = "Iraq" },
+              new Student { Id = 6, Age = 19, Name = "Ramii", Country = "Lebanon" },
+              new Student { Id = 7, Age = 44, Name = "Osama", Country = "Egypt" },
+              new Student { Id = 8, Age = 34, Name = "Abbas", Country = "Yemen" },
+            };
+        }
+    }
+}
