20bc3d027ee826e9967c9dfb2d97e79f16c8e66a
Youssef Abbas <youssefabbasawad@gmail.com>
2024-07-18 07:49:48 +0330

    Join Method Lesson

---
+++ JoinMethod/BookWithAuthor.cs
@@ -0,0 +1,15 @@
+﻿namespace JoinMethod
+{
+    public class BookWithAuthor
+    {
+        public required int BookId { get; set; }
+        public required string BookTitle { get; set; }
+        public required string BookAuthor { get; set; }
+        public required double BookPrice { get; set; }
+
+        public override string ToString()
+        {
+            return $"BookId: {BookId}, Title: {BookTitle}, Author: {BookAuthor}, Price: ${BookPrice}";
+        }
+    }
+}
--- JoinMethod/Program.cs
+++ JoinMethod/Program.cs
@@ -5,6 +5,35 @@ IEnumerable<Book> books = DataSource.GetBooks();
 IEnumerable<Author> authors = DataSource.GetAuthors();
 
 // Method Syntax
+IEnumerable<BookWithAuthor> booksWithAuthor = books.Join( // outer collection
+     authors, // inner collection
+     book => book.AuthorId, // outer key selector
+     author => author.Id, // inner key selector
+     (book, author) => new BookWithAuthor // result selector
+     {
+         BookId = book.Id,
+         BookTitle = book.Title,
+         BookPrice = book.Price,
+         BookAuthor = author.FullName,
+     });
+
+
 // Query Syntax
+IEnumerable<BookWithAuthor> booksWithAuthorQuery = from book in books // outer collection
+                                                   join author in authors // inner collection
+                                                   on book.AuthorId equals author.Id  // key selectors
+                                                   select new BookWithAuthor // result selector
+                                                   {
+                                                       BookId = book.Id,
+                                                       BookTitle = book.Title,
+                                                       BookPrice = book.Price,
+                                                       BookAuthor = author.FullName,
+                                                   };
+
+
+foreach (var bookWithAuthor in booksWithAuthorQuery)
+    Console.WriteLine(bookWithAuthor.ToString());
+
+
 
 Console.ReadKey();
