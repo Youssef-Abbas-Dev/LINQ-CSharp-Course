734a88d6045f86105ccc76d63bde1d0b6193ca93
Youssef Abbas <youssefabbasawad@gmail.com>
2024-08-02 07:31:54 +0330

    LINQ Equality Operator

---
+++ EqualityOperatorExample/Product.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace EqualityOperatorExample
+{
+    public class Product
+    {
+        public required string Title { get; set; }
+        public required double Price { get; set; }
+    }
+
+    public class ProductComparer : IEqualityComparer<Product>
+    {
+        public bool Equals(Product? productOne, Product? productTwo)
+        {
+            if (productOne == null || productTwo == null) return false;
+
+            if (productOne.Title == productTwo.Title && productOne.Price == productTwo.Price)
+                return true;
+
+            return false;
+        }
+
+        public int GetHashCode([DisallowNull] Product obj)
+        {
+            return obj.GetHashCode();
+        }
+    }
+}
---
+++ EqualityOperatorExample/Program.cs
@@ -0,0 +1,28 @@
+﻿// LINQ Equality Operators: SequenceEqual
+using EqualityOperatorExample;
+
+//var letters = new List<char> { 'A', 'B', 'C', 'D' };
+//var lettersTwo = new List<char> { 'A', 'B', 'C', 'D' };
+
+//bool isEqual = letters.SequenceEqual(lettersTwo);
+//Console.WriteLine(isEqual);
+
+var products = new List<Product>()
+{
+    new Product { Title = "Book", Price = 10.99 },
+    new Product { Title = "Pen", Price = 2.99 },
+    new Product { Title = "Laptop", Price = 400.99 },
+};
+
+var productsTwo = new List<Product>()
+{
+    new Product { Title = "Book", Price = 10.99 },
+    new Product { Title = "Pen", Price = 2.99 },
+    new Product { Title = "Laptop", Price = 400.99 },
+};
+
+bool isProductsEqual = products.SequenceEqual(productsTwo, new ProductComparer());
+Console.WriteLine(isProductsEqual);
+
+
+Console.ReadKey();
