4bbc67b4b8c963f5eaebc3ce89907786f86a9cb5
Youssef Abbas <youssefabbasawad@gmail.com>
2024-08-07 07:22:52 +0330

    Generation Operation Example

---
+++ GenerationOperatorsExample/Program.cs
@@ -0,0 +1,17 @@
+﻿// Generation Operators
+
+// Empty
+//var emptyCollection = Enumerable.Empty<int>();
+//Console.WriteLine(emptyCollection.Count());
+
+// Range
+//var rangeCollection = Enumerable.Range(5, 10);
+//foreach (var range in rangeCollection)
+//    Console.WriteLine(range);
+
+// Repeat
+var repeatCollection = Enumerable.Repeat<string>("Hi", 10);
+foreach (var item in repeatCollection)
+    Console.WriteLine(item);
+
+Console.ReadKey();
